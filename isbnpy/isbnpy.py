ac06b421a8a747cb15e14f07462b74d028ae9e55
= <=>
2023-02-23 11:30:08 -0300

    manejo de errores

--- isbnpy/isbnpy.py
+++ isbnpy/isbnpy.py
@@ -12,30 +12,35 @@ class ISBNpy:
            'Cookie': self.cookie,
            'Referer': f'https://isbnsearch.org/isbn/{isbn}'
             }
-        page = get(f"{BASE_URL}/{isbn}", headers=headers)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        res = soup.find('div', class_='bookinfo')
-        #text = res.find('h1')
-        title = res.find('h1').get_text()
-        _p = []
-        for p in res.find_all('p'):
-            _p.append(p.get_text())
+        try:
+            page = get(f"{BASE_URL}/{isbn}", headers=headers)
+            if page.status_code == 403:
+                return {'msg': 'cookie expired', 'status_code': 403}
+            soup = BeautifulSoup(page.content, 'html.parser')
+            res = soup.find('div', class_='bookinfo')
+            title = res.find('h1').get_text()
+            _p = []
+            for p in res.find_all('p'):
+                _p.append(p.get_text())
 
-        isbn_13 = _p[0].split('ISBN-13: ')[1].strip()
-        isbn_10 = _p[1].split('ISBN-10: ')[1].strip()
-        author = _p[2].split('Author:')[1].strip()
-        edition = _p[3].split('Edition:')[1].strip()
-        binding = _p[4].split('Binding:')[1].strip()
-        publisher = _p[5].split('Publisher:')[1].strip()
-        published = _p[6].split('Published:')[1].strip()
-        _json = {
-            'title': title,
-            'isbn_13': isbn_13,
-            'isbn_10': isbn_10,
-            'author': author,
-            'edition': edition,
-            'binding': binding,
-            'publisher': publisher,
-            'published': published
-        }
-        return _json
+            isbn_13 = _p[0].split('ISBN-13: ')[1].strip()
+            isbn_10 = _p[1].split('ISBN-10: ')[1].strip()
+            author = _p[2].split('Author:')[1].strip()
+            edition = _p[3].split('Edition:')[1].strip()
+            binding = _p[4].split('Binding:')[1].strip()
+            publisher = _p[5].split('Publisher:')[1].strip()
+            published = _p[6].split('Published:')[1].strip()
+            _json = {
+                'title': title,
+                'isbn_13': isbn_13,
+                'isbn_10': isbn_10,
+                'author': author,
+                'edition': edition,
+                'binding': binding,
+                'publisher': publisher,
+                'published': published,
+                'status_code': 200
+            }
+            return _json
+        except:
+            return {'msg': 'isbn not found', 'status_code':404}
